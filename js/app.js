188b938cc4147a5c295439f6589fd5101603dfdd
CarissaAyo <ajaoyussufayo@gmail.com>
2020-09-24 21:42:39 +0100

    the navbar code

--- js/app.js
+++ js/app.js
@@ -1,11 +1,23 @@
 let harmburger = document.getElementById('harmburger');
 let closeButton = document.getElementById('close-menu');
-let showNav = document.getElementById('show-nav')
+let showNav= document.querySelector('.show-nav');
 harmburger.addEventListener('click',function(){
+    // to remove the harmburger image
     harmburger.classList += "close-harmburger";
+    // to add the close image
     closeButton.classList.toggle('close-img');
+    // to show the nav-menu
     showNav.classList.toggle('hide-nav')
 
 
 })
-console.log(closeButton);
+closeButton.addEventListener('click',function(){
+    // to show the harmburger image
+    harmburger.classList.toggle('close-harmburger');
+    // to remove the close image
+    closeButton.classList.toggle('close-img');
+    // to close the nav-menu
+    showNav.classList.toggle('hide-nav')
+})
+
+
