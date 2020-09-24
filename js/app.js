de08837f61472bc109deae6a07396cf99a7eaab0
CarissaAyo <ajaoyussufayo@gmail.com>
2020-09-24 17:18:22 +0100

    Still working on js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-let close
+let harmburger = document.getElementById('harmburger');
+let closeButton = document.getElementById('close-menu');
+let showNav = document.getElementById('show-nav')
+harmburger.addEventListener('click',function(){
+    harmburger.classList += "close-harmburger";
+    closeButton.classList.toggle('close-img');
+    showNav.classList.toggle('hide-nav')
+
+
+})
+console.log(closeButton);
