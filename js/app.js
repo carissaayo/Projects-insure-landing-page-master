49f73c66752150ccb4ad9672723a99de1a178669
CarissaAyo <ajaoyussufayo@gmail.com>
2021-04-26 15:16:52 +0100

    fixed the mobile-nav bug

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
-let mobileNav = document.querySelector("#nav-con");
 let closeButton = document.querySelector(".cross");
 let hamburger = document.getElementById("hamburger");
+let mobileNav = document.querySelector(".mobile-nav");
 
 hamburger.addEventListener("click", ()=>{
-    mobileNav.classList.toggle("show-nav");
+    mobileNav.classList.toggle("show-nav-con");
+    console.log(mobileNav);
     closeButton.classList.toggle('close-menu')
 
 });
 
 closeButton.addEventListener("click",()=>{
-    mobileNav.classList.toggle("show-nav");
+    mobileNav.classList.toggle("show-nav-con");
     closeButton.classList.toggle("close-menu");
 });
