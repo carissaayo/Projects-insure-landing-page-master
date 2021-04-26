a7c24ebe416ef896683b6174ec5004dbcda7985a
CarissaAyo <ajaoyussufayo@gmail.com>
2021-04-26 14:02:23 +0100

    done with the project

--- js/app.js
+++ js/app.js
@@ -1,33 +1,14 @@
-let harmburger = document.getElementById('harmburger');
-let closeButton = document.getElementById('close-menu');
-let showNav= document.querySelector('.show-nav');
-let links = document.querySelectorAll('#desktop-nav ul li')
-harmburger.addEventListener('click',function(){
-    // to remove the harmburger image
-    harmburger.classList += "close-harmburger";
-    // to add the close image
-    closeButton.classList.toggle('close-img');
-    // to show the nav-menu
-    showNav.classList.toggle('hide-nav')
+let mobileNav = document.querySelector("#nav-con");
+let closeButton = document.querySelector(".cross");
+let hamburger = document.getElementById("hamburger");
 
+hamburger.addEventListener("click", ()=>{
+    mobileNav.classList.toggle("show-nav");
+    closeButton.classList.toggle('close-menu')
 
-})
-closeButton.addEventListener('click',function(){
-    // to show the harmburger image
-    harmburger.classList.toggle('close-harmburger');
-    // to remove the close image
-    closeButton.classList.toggle('close-img');
-    // to close the nav-menu
-    showNav.classList.toggle('hide-nav')
-})
+});
 
-Array.from(links).forEach((link)=>{
-    link.addEventListener('click',function(e){
-        e.target.style.backgroundColor = 'hsl(256, 26%, 20%)';
-        e.target.style.color='white'
-    });
-    // link.
-}
-)
-
-
+closeButton.addEventListener("click",()=>{
+    mobileNav.classList.toggle("show-nav");
+    closeButton.classList.toggle("close-menu");
+});
